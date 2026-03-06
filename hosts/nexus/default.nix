752fa7a47a7a6450fc3cb5cf357b63a894a30e14
misfit <lao.soufiane@proton.me>
2026-03-06 14:44:09 +0000

    update

--- hosts/nexus/default.nix
+++ hosts/nexus/default.nix
@@ -147,17 +147,7 @@
     port = 22;
     openFirewall = true;
   };
-  networking.interfaces.eth0.ipv4.addresses = [
-    {
-      address = "192.168.1.50";
-      prefixLength = 24;
-    }
-  ];
-  networking.defaultGateway = "192.168.1.1";
-  networking.nameservers = [
-    "1.1.1.1"
-    "8.8.8.8"
-  ];
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
